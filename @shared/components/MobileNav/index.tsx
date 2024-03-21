a767611a007d6b70718c8d95648b6e72ec4f3f17
perebonilado <reradiri@eminenttechnology.com>
2024-03-21 17:59:23 +0100

    removed fade transition

--- @shared/components/MobileNav/index.tsx
+++ @shared/components/MobileNav/index.tsx
@@ -28,42 +28,40 @@ const MobileNav: FC<Props> = ({ handleClose, isSideNavOpen }) => {
   }, []);
 
   return (
-    <FadeInTransition>
-      <div
-        className={
-          "bg-[#020228] overflow-x-hidden rounded-md transition-all w-[90vw] max-w-[350px] h-[calc(100vh-100px)] z-[6000] fixed top-[90px] left-1/2 -translate-x-1/2 flex flex-col justify-between"
-        }
-      >
-        <div className="flex flex-col h-full pt-12">
-          <div style={{ flex: 4 }}>
-            <AnimateRightTransition>
-              {linkKeys.map((lk, idx) => {
-                return (
-                  <MobileNavItem
-                    key={idx}
-                    link={NavLinks[lk].link}
-                    title={NavLinks[lk].title}
-                  />
-                );
-              })}
-            </AnimateRightTransition>
-          </div>
-          <div
-            style={{ flex: 1 }}
-            className="py-2 px-5 flex flex-col justify-end gap-3"
-          >
-            <p className="text-white font-semibold">Follow us on:</p>
-            <div className="flex items-center gap-3">
-              <FacebookIcon />
+    <div
+      className={
+        "bg-[#020228] overflow-x-hidden rounded-md transition-all w-[90vw] max-w-[350px] h-[calc(100vh-100px)] z-[6000] fixed top-[90px] left-1/2 -translate-x-1/2 flex flex-col justify-between"
+      }
+    >
+      <div className="flex flex-col h-full pt-12">
+        <div style={{ flex: 4 }}>
+          <AnimateRightTransition>
+            {linkKeys.map((lk, idx) => {
+              return (
+                <MobileNavItem
+                  key={idx}
+                  link={NavLinks[lk].link}
+                  title={NavLinks[lk].title}
+                />
+              );
+            })}
+          </AnimateRightTransition>
+        </div>
+        <div
+          style={{ flex: 1 }}
+          className="py-2 px-5 flex flex-col justify-end gap-3"
+        >
+          <p className="text-white font-semibold">Follow us on:</p>
+          <div className="flex items-center gap-3">
+            <FacebookIcon />
 
-              <LinkedInIcon />
+            <LinkedInIcon />
 
-              <InstagramIcon />
-            </div>
+            <InstagramIcon />
           </div>
         </div>
       </div>
-    </FadeInTransition>
+    </div>
   );
 };
 
