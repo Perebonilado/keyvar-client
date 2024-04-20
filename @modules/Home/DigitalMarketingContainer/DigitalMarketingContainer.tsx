db023606a69cc1bb800ed72c97cfb2e034cbeaf3
perebonilado <reradiri@eminenttechnology.com>
2024-04-20 08:34:44 +0100

    founders container

--- @modules/Home/DigitalMarketingContainer/DigitalMarketingContainer.tsx
+++ @modules/Home/DigitalMarketingContainer/DigitalMarketingContainer.tsx
@@ -1,14 +1,17 @@
+import Avatar from "@/@shared/components/Avatar";
 import { NavLinks } from "@/@shared/constants";
 import Button from "@/@shared/ui-components/Button";
 import Container from "@/@shared/ui-components/Container";
 import Link from "next/link";
 import React, { FC } from "react";
+import Image from "next/image";
+import s from "./styles.module.css";
 
 const DigitalMarketingContainer: FC = () => {
   return (
-    <section className="py-20 min-h-screen">
+    <section className="py-20 relative">
       <Container>
-        <div className="flex max-md:flex-col pt-10">
+        <div className="flex max-md:flex-col pt-10 max-md:gap-16">
           <div style={{ flex: 1 }}>
             <div className="w-full max-w-[600px]">
               <h2 className="text-5xl font-bold leading-relaxed mb-4 max-md:text-3xl">
@@ -33,7 +36,33 @@ const DigitalMarketingContainer: FC = () => {
             </div>
           </div>
 
-          <div style={{ flex: 1 }}></div>
+          <div
+            style={{ flex: 1 }}
+            className={`flex flex-col justify-end items-end mt-[250px] max-md:mt-0 ${s.founderBgContainer}`}
+          >
+            <div className="flex flex-col items-end pb-10">
+              <Avatar
+                alt="Founder's Image: Kalada Nemieboka"
+                fallBack="Kalada"
+                imageUrl="/assets/about-us/kalada.png"
+                size="lg"
+              />
+              <p className="mt-3 font-bold">Kalada Nemieboka</p>
+              <p className="text-sm">Founder and Senior Analytics Consultant</p>
+            </div>
+
+            <div className={`absolute w-full h-[400px] left-0`}>
+              <div className="relative w-full h-full">
+                <Image
+                  layout="fill"
+                  objectFit="contain"
+                  objectPosition="100% 100%"
+                  alt="background image"
+                  src={"/assets/shared/founder-bg.png"}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </Container>
     </section>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Banner from "@/@modules/Home/Banner/Banner";
-import DigitalMarketingContainer from "@/@modules/Home/DigitalMarketingContainer";
+import DigitalMarketingContainer from "@/@modules/Home/DigitalMarketingContainer/DigitalMarketingContainer";
 import HelpClients from "@/@modules/Home/HelpClients/HelpClients";
 import AppHead from "@/@shared/components/AppHead";
 import AppLayout from "@/@shared/layout/AppLayout";
